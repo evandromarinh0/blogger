ff950081ab9dbedef809046a09d292df6ac80dfc
Evandro Marinho <evanforgotlastone@gmail.com>
2025-05-13 02:18:54 -0300

    feat: header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { useRouter } from "next/router";
+import { cn } from "@/lib/utils";
+
+export function Header() {
+  const router = useRouter();
+
+  return(
+    <header className="fixed top-0 z-1 w-full border-b border-white/10 bg-foreground/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <Link href="/">Logo</Link>
+
+          <nav className="flex items-center gap-6">
+            <Link href="/" className={cn('text-sm font-medium transition-colors hover:text-primary', router.pathname === '/' ? 'text-blue-500' : 'text-muted-foreground')}>Início</Link>
+            <Link href="/blog" className={cn('text-sm font-medium transition-colors hover:text-primary', router.pathname.startsWith('/blog') ? 'text-blue-500' : 'text-muted-foreground')}>Blog</Link>
+            <Button variant="secondary">Começar</Button>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="ghost">Button</Button>
-    </div>
+    <></>
   );
 }

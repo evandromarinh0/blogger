c36e302e0416e9dfa996f5730c8dde13310be239
Evandro Marinho <evanforgotlastone@gmail.com>
2025-05-13 01:44:13 -0300

    feat: shadcn ui installed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h2 className="text-4xl text-red-300">Hello World</h2>
+      <Button variant="ghost">Button</Button>
     </div>
   );
 }

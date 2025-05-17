80b071052cd5abda725162cde561346751fd0b70
Evandro Marinho <evanforgotlastone@gmail.com>
2025-05-17 05:26:54 -0300

    feat: support section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { FeatureSection } from "@/components/feature-section";
 import { HeroSection } from "@/components/hero-section";
+import { SupportSection } from "@/components/support-section";
 
 export default function Home() {
   return (
     <article className="flex flex-col bg-gray-700">
       <HeroSection />
       <FeatureSection />
+      <SupportSection />
     </article>
   );
 }

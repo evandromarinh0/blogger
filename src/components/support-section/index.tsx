d3da01559c0f54211e192da12a37d8a65552faa8
Evandro Marinho <evanforgotlastone@gmail.com>
2025-05-17 19:57:59 -0300

    chore: more css adjustment

--- src/components/support-section/index.tsx
+++ src/components/support-section/index.tsx
@@ -5,8 +5,8 @@ const ptSansCaption = PT_Sans_Caption({ subsets: ['latin'], weight: '700' });
 
 export function SupportSection(){
   return(
-    <section className="pb-8 md:py-10 bg-[url('/support-section.png')] bg-[position:center_center] bg-cover bg-no-repeat w-full h-556">
-      <div className="container flex flex-col items-center gap-12 py-32">
+    <section className="pb-8 bg-[url('/support-section.png')] bg-[position:center_center] bg-cover bg-no-repeat w-full h-556">
+      <div className="container flex flex-col items-center gap-12 pt-12 md:py-32">
         <h2 className={`${ptSansCaption.className} text-balance text-center text-heading-xl text-gray-100`}>Sua loja de afiliados, simples, do jeito que deveria ser</h2>
 
         <div className="grid gap-6 md:grid-cols-3">
